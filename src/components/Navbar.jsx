9ced068275eef7c3b33df716c269ca93340d873a
liyuKnow <kidus2011@gmail.com>
2022-09-10 14:11:48 -0700

    mobile navbar hover effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { close, logo, menu} from '../assets';
 import { navLinks } from '../constants';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
@@ -19,6 +21,31 @@ const Navbar = () => {
           ))
         }
       </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img 
+          src={ toggle ? close : menu} 
+          alt="" 
+          className='w-[24px] h-[24px] object-contain'
+          onClick={ ()=> setToggle((prev) => (!prev))}
+        />
+          <div
+            className={`${toggle ? 'flex' : 'hidden'} bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          >
+            <ul className='w-full list-none flex flex-col justify-end items-center text-center flex-1'>
+              {
+                navLinks?.map((nav,index) => (
+                  <li
+                    key={index}
+                    className={`w-full font-poppins font-normal cursor-pointer text-[16px] text-white p-2  hover:bg-gray-600`}
+                  >
+                    <a href={`#${nav.id}`}>{nav.title}</a>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+      </div>
     </nav>
   )
 }
